6149bc6560f4ed3234485cb5373b8e7c2dc1c0cc
Student <example@example.com>
2020-01-15 14:57:43 -0800

    add cup to gal and gal to cup conversions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+function cupCon(numberOfCups){
+return (numberOfCups * 0.0625)
+}
+function galCon(numberofGals){
+return (numberofGals * 16)
+}
+
+
+
+
+
+
 $(document).ready(function(){
+ 
+
+    $("#cup2galForm").submit(function(){
+        var cup2gal = parseFloat($("input#cup2gal").val());
+        var cup2galConversion = cupCon(cup2gal)
+        $("#cupsResult").show();
+        event.preventDefault();
+        $(".cupsResult").text(cup2galConversion);
+    });
+    $("#gal2cupForm").submit(function(){
+        var gal2cup = parseFloat($("input#gal2cup").val());
+        var gal2cupConversion = galCon(gal2cup)
+        $("#galsResult").show();
+        event.preventDefault();
+        $(".galsResult").text(gal2cupConversion);
+    });
+   
 
 });
